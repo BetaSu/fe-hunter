8e1fcd05280dc1277227f7e5bd2c8b08b08eb3b2
kasong <313439271@qq.com>
2022-03-25 12:42:07 +0800

    feat: add ask cli

---
+++ cli/ask.js
@@ -0,0 +1,53 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+const templatesPath = path.resolve(__dirname, '../templates');
+const questionsPath = path.resolve(__dirname, '../questions');
+
+const questions = [
+  {
+    type: 'list',
+    name: 'type',
+    message: '问题的类型是什么？',
+    choices: [
+      '场景题：发生在某一场景下的技术问题', 
+      '动手题：写代码实现某一功能', 
+      '职场题：程序员在职场中会遇到的问题'
+    ],
+    filter(val) {
+      return val.replace(/(.+)：.+/, function(_, type) {
+        console.log('tt', type);
+        return type;
+    })
+    },
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: '要提的问题是？',
+    validate(value, {type}) {
+      if (type !== '动手题' && (
+        !value.endsWith('？') &&
+        !value.endsWith('?')
+      )) {
+        return '问题请以？结尾'
+      }
+      return true;
+    },
+    filter: String,
+  }
+];
+
+inquirer.prompt(questions).then(({type, title}) => {
+  const tempList = fs.readdirSync(templatesPath);
+  tempList.forEach(tempName => {
+    if (!tempName.includes(type)) {
+      return;
+    }
+    const tempContent = fs.readFileSync(path.resolve(templatesPath, `./${tempName}`), {encoding: 'utf-8'});
+    const targetQuestionPath = path.resolve(questionsPath, `./${title}.md`);
+    fs.writeFileSync(targetQuestionPath, tempContent);
+    console.log(`✅ 请在 ${targetQuestionPath} 中编辑问题`);
+  })
+});
