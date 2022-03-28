89711d1292fc7d40151f1411bfb3f9c68b82c38d
kasong <313439271@qq.com>
2022-03-28 09:31:33 +0800

    feat: add qus

--- cli/ask.js
+++ cli/ask.js
@@ -13,11 +13,11 @@ const questions = [
     choices: [
       '场景题：发生在某一场景下的技术问题', 
       '动手题：写代码实现某一功能', 
-      '职场题：程序员在职场中会遇到的问题'
+      '职场题：程序员在职场中会遇到的问题',
+      '基础题：经典面试题、基础前端问题'
     ],
     filter(val) {
       return val.replace(/(.+)：.+/, function(_, type) {
-        console.log('tt', type);
         return type;
     })
     },
@@ -27,6 +27,9 @@ const questions = [
     name: 'title',
     message: '要提的问题是？',
     validate(value, {type}) {
+      if (type === '动手题' && !value.startsWith('实现')) {
+        return '动手题标题格式为：实现xxxxx'
+      }
       if (type !== '动手题' && (
         !value.endsWith('？') &&
         !value.endsWith('?')
